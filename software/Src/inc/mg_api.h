ce30719f094826ec4f3c45ba8e2c4f29f6191c5f
macrogiga <zhmchen@macrogiga.com>
2019-08-27 14:33:18 +0800

    use updated ble lib v5.2.4.

--- software/Src/inc/mg_api.h
+++ software/Src/inc/mg_api.h
@@ -21,7 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
     DEALINGS IN THE SOFTWARE.
 */
-/*lib release: v5.2.1*/
+/*lib release: v5.2.4*/
 
 #ifndef _MG_API_H_
 #define _MG_API_H_
@@ -72,6 +72,11 @@ void radio_initBle(unsigned char txpwr, unsigned char** addr/*out*/);  //init bl
 //return: 0 - fail;  none 0 - success
 unsigned char radio_initBle_TO(unsigned char txpwr, unsigned char** addr, unsigned short ms_timeout);
 
+//Function: radio_initBle_recover
+//Parameters: txpwr - input, txpower;   addr - output, BLE device address
+//return: None
+void radio_initBle_recover(unsigned char txpwr, unsigned char** addr);
+
 //Function: radio_setCal_nonBlocking
 //Parameters: nonblocking - 0: blocking;  1: non blocking
 //return: none
@@ -81,6 +86,7 @@ void radio_setCal_nonBlocking(unsigned nonblocking);
 //this function is to set rf to standby mode, I ~ 3uA
 //Parameters: none
 //return: None
+//called in UsrProcCallback or when ble_run_interrupt_McuCanSleep()>0
 void radio_standby(void);
 
 //Function: radio_resume
@@ -199,9 +205,23 @@ void SetLePinCode(unsigned char *PinCode/*6 0~9 digitals*/);
 //  2. This function is ONLY supported in pairing cases.
 unsigned char* GetLTKInfo(unsigned char* newFlag);
 
+//security manager module request for pair
+//Remarks:
+//  1. This function shall be invoked when [connected status == 1].
+//  2. This function is ONLY supported in pairing cases.
+void s_llSmSecurityReq(void);
 
 
 
+
+///////////////////////////OTA APIs/////////////////////////////////
+//return: 
+// OTA_OK             0
+// OTA_SN_ERROR       1
+// OTA_CHECKSUM_ERROR 2
+// OTA_FORMAT_ERROR   3
+// OTA_UNKNOWN_ERROR  255
+unsigned char OTA_Proc(unsigned char *data, unsigned short len);
 
 
 ///////////////////////////interrupt running mode APIs/////////////////////////////////
@@ -307,5 +327,11 @@ void SetFixAdvChannel(unsigned char isFixCh37Flag);
 //unsigned char aes_encrypt_HW(unsigned char *painText128bitBE,unsigned char *key128bitBE); //porting api, returns zero means not supported
 
 //void McuGotoSleepAndWakeup(void);
+
+
+////////////////////////For OTA application use//////////////////////////
+//void OtaSystemReboot(void);
+//void WriteFlashE2PROM(u8* data, u16 len, u32 pos, u8 flag); //4 bytes aligned
+//u32 GetOtaAddr(void);
 
 #endif
